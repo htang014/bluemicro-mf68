d3f8a5a4c19b1ebe6044cdb191075260a0abc8fd
Hans Tang <hanstang007@gmail.com>
2021-08-03 14:38:59 -0700

    Update keymap.cpp

--- 4x4Tutorials-base-single-nice_nano/keymap.cpp
+++ 4x4Tutorials-base-single-nice_nano/keymap.cpp
@@ -18,7 +18,7 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 #include "keymap.h"
 
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
-    {(KEYMAP(
+    {KEYMAP(
         KC_ESC,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,
         KC_9,         KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_INSERT,  KC_PGUP,    KC_TAB,     KC_Q,
         KC_W,         KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,
