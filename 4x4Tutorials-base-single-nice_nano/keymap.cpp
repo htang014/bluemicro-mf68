25bd2b36f4257841c522767fdcedb4db5ee215db
Hans Tang <hanstang007@gmail.com>
2021-08-05 18:47:53 -0700

    Update keymap.cpp

--- 4x4Tutorials-base-single-nice_nano/keymap.cpp
+++ 4x4Tutorials-base-single-nice_nano/keymap.cpp
@@ -33,7 +33,7 @@ std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
 void setupKeymap() {
     uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] = KEYMAP(
         KC_NO,       KC_NO,       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,     
-        KC_NO,       KC_NO,       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+        KC_NO,       KC_NO,       KC_NO,      KC_NO,      RESET,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
         KC_NO,       KC_NO,       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
         KC_NO,       KC_NO,       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
         KC_NO,       KC_NO,       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
